68eba52657f8066f49546cfca2e578246d5b3c2d
TylerErdman <tyler.erdman@my.simpson.edu>
2020-08-27 15:14:25 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-Hi
+print("Good Golly, I enjoy spending time with my cats and my family.")
+print("""Other times I feel like I just want some quiet time.
+But otherwise I just want to scream to the world.
+\"Please love me, but not all the time!\"""")
