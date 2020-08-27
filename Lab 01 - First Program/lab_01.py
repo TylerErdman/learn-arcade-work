b9b54a8761b41a85b0e0028df86e87cd5359f528
TylerErdman <tyler.erdman@my.simpson.edu>
2020-08-27 15:45:15 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -2,4 +2,4 @@ print("\tGood Golly, I enjoy spending time with my cats and my family.")
 print("""Other times I feel like I just want some quiet time.
 But otherwise I just want to scream to the world.
 \"Please love me, but not all the time!\"""")
-print('This great story was brought to you be me...\nTyler Erdman!')
+print('This great story was brought to you be me...\nTyler Erdman!!')
