04a4957546f86268ad912f9053b6005beed6c871
TylerErdman <tyler.erdman@my.simpson.edu>
2020-11-09 16:06:52 -0600

    Version 1 of rooms in the house

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -16,11 +16,11 @@ class Room:
 
 class Item:
     """Defines an Item class."""
-    def __init__(self, room_number, long_description, short_name, picked_up):
+    def __init__(self, room_number, long_description, short_name):
         self.room_number = room_number
         self.long_description = long_description
         self.short_name = short_name
-        self.picked_up = picked_up
+        self.picked_up = False
 
 
 def main():
@@ -90,7 +90,8 @@ def main():
                 "\nIt seems the only way out the north to the top of the stairs.", 9, None, None, None, None, None)
     room_list.append(room)
 
-    room = Room("Standing in the basement it looks like a chamber with a door to the south and north",
+    room = Room("Standing in the basement it looks like a chamber with a door to the south and north."
+                "\nThere's also the stairs to the west or up that lead to the office",
                 15, 14, None, 4, 4, None)
     room_list.append(room)
 
@@ -98,13 +99,85 @@ def main():
                 "\nThere is just enough space to open the door.", 13, None, None, None, None, None)
     room_list.append(room)
 
+    room = Room("You're in a large stone hallway. You can hear water dripping."
+                "\nThere are doors on the east and west walls"
+                "\nThe door to the south leads to the chamber.", None, 13, 16, 17, None, None)
+    room_list.append(room)
+
+    room = Room("The room is very dark and damp. You see a large metal table in the center covered with blood."
+                "\nThe chains attached to the ceiling are swaying slowly back and forth."
+                "\nIt seems the only exit is back through the door, west.", None, None, None, 15, None, None)
+    room_list.append(room)
+
+    room = Room("There is a large sleeping bear in the *corner of the room. It seems to be a cave."
+                "\nThere is the doorway to the east."
+                "\nYou can feel a breeze coming from the tunnel to the south.", None, 18, 15, None, None, None)
+    room_list.append(room)
+
+    room = Room("It is a dark tunnel with a light at the end of it."
+                "\nYou are sure you can get out if you keep going south. You can also go back north to the bear room.",
+                17, 19, None, None, None, None)
+    room_list.append(room)
+
+    room = Room("You have finally escaped that creepy house.", None, None, None, None, None, None)
+    room_list.append(room)
 
     item_list = []
-    item = Item()
+    item = Item(5, "A short blunt butter knife, good for spreading jam.", "KNIFE")
     item_list.append(item)
 
     current_room = 0
     done = False
+
+    while not done:
+        print()
+        print(room_list[current_room].description)
+        print()
+        print("You can type q or quit to exit the game.")
+        print()
+        direction_traveled = str(input("Which way would you like to go? "))
+        if direction_traveled.upper() == "N" or direction_traveled.upper() == "NORTH":
+            next_room = room_list[current_room].north
+            if next_room is None:
+                print("You can't go that way")
+            else:
+                current_room = next_room
+        elif direction_traveled.upper() == "S" or direction_traveled.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+            if next_room is None:
+                print("You can't go that way")
+            else:
+                current_room = next_room
+        elif direction_traveled.upper() == "E" or direction_traveled.upper() == "EAST":
+            next_room = room_list[current_room].east
+            if next_room is None:
+                print("You can't go that way")
+            else:
+                current_room = next_room
+        elif direction_traveled.upper() == "W" or direction_traveled.upper() == "WEST":
+            next_room = room_list[current_room].west
+            if next_room is None:
+                print("You can't go that way")
+            else:
+                current_room = next_room
+        elif direction_traveled.upper() == "U" or direction_traveled.upper() == "UP":
+            next_room = room_list[current_room].up
+            if next_room is None:
+                print("You can't go that way")
+            else:
+                current_room = next_room
+        elif direction_traveled.upper() == "D" or direction_traveled.upper() == "DOWN":
+            next_room = room_list[current_room].down
+            if next_room is None:
+                print("You cant go that way")
+            else:
+                current_room = next_room
+        elif direction_traveled.upper() == "Q" or direction_traveled.upper() == "QUIT":
+            print("Thanks for playing!")
+            done = True
+        else:
+            print()
+            print("I don't understand that.")
 
 
 if __name__ == "__main__":
